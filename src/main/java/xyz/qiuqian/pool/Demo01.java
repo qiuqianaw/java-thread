75702b385be46178ddefdd075da8f81ecb6b12b5
qiuqianaw <qiuqianaw@163.com>
2022-02-12 13:24:43 +0800

    pool ok

--- src/main/java/xyz/qiuqian/pool/Demo01.java
+++ src/main/java/xyz/qiuqian/pool/Demo01.java
@@ -1,17 +1,31 @@
 package xyz.qiuqian.pool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 // Executors 工具类，三大方法
 public class Demo01 {
     public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newSingleThreadExecutor();// 单个线程
-//        ExecutorService threadPool = Executors.newFixedThreadPool(5);// 创建一个固定的线程池的大小
-//        ExecutorService threadPool = Executors.newCachedThreadPool();// 可伸缩的
+        // 获取 CPU 核心数量
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        // 自定义线程池
+        /**
+         * 银行业务窗口
+         *  两个开放，三个不开放
+         */
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                3,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
 
         try {
-            for (int i = 0; i < 100; i++) {
+            // 最大承载 = 队列 + max值
+            // 超出最大承载被拒绝策略拒绝，且抛出异常 java.util.concurrent.RejectedExecutionException
+            for (int i = 1; i <= 5; i++) {
     //            new Thread().start(); // 传统方式
                 // 使用线程池创建线程
                 threadPool.execute(() -> {
